84f992f2ae4f34c16962e6b349bead00794d23f8
Zayd <zaidbaig738@gmail.com>
2025-02-18 10:46:56 +0530

    frd

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.get("/", (req, res) => {
 app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Route not found" });
-});
+// app.use((req, res, next) => {
+//   res.status(404).json({ error: "Route not found" });
+// });
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: "Internal server error" });
-});
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).json({ error: "Internal server error" });
+// });
 
 const startServer = async () => {
   try {
