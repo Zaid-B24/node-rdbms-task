0161e6361b8ee68124d207482d5ea5490c987f7c
Zayd <zaidbaig738@gmail.com>
2025-02-18 06:41:47 +0530

    build

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
+});
+
 app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
 
